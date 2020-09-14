5266184ac046d55fa29a1c407f1ab5151a16cd1f
Rene Straub <straub@see5.ch>
2020-09-14 21:19:20 +0200

    Refactor UBX-CFG-RATE to use  serializer

--- src/ubx_cfg_rate.rs
+++ src/ubx_cfg_rate.rs
@@ -1,10 +1,9 @@
 use std::fmt;
 
-use crate::cid::UbxCID as UbxCID;
-use crate::frame::UbxFrame as UbxFrame;
-use crate::frame::UbxFrameInfo as UbxFrameInfo;
-use crate::frame::UbxFrameSerialize as UbxFrameSerialize;
-use crate::frame::UbxFrameDeSerialize as UbxFrameDeSerialize;
+use serde::{Serialize, Deserialize};
+
+use crate::cid::UbxCID;
+use crate::frame::{UbxFrame, UbxFrameInfo, UbxFrameSerialize, UbxFrameDeSerialize};
 
 
 const CLS: u8 = 0x06;
@@ -38,31 +37,29 @@ impl UbxFrameInfo for UbxCfgRatePoll {
 
 impl UbxFrameSerialize for UbxCfgRatePoll {
     fn to_bin(&self) -> Vec<u8> {
-        // poll frame has no data to serialize
-        // just build empty frame
-        let frame = UbxFrame::construct(
-            UbxCID::new(CLS, ID), 
-            [].to_vec());
+        let frame = UbxFrame::construct(UbxCID::new(CLS, ID), [].to_vec());
         let msg = frame.to_bytes();
         msg
     }
 }
 
 
-
 #[derive(Default)]
-pub struct UbxCfgRate {
-    pub name: &'static str,
-    cid: UbxCID,
-
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
     pub meas_rate: u16,  // Time elapsed between two measuremnts in ms
     pub nav_rate: u16,   // Number of measurements for NAV solution
     pub time_ref: u16,
 }
 
+#[derive(Default)]
+pub struct UbxCfgRate {
+    pub name: &'static str,
+    cid: UbxCID,
+    pub data: Data,
+}
+
 impl UbxCfgRate {
-    // TODO: ctor to create frame with data
-    // loads frame ...
     pub fn new() -> Self {
         Self { 
             name: "UBX-CFG-RATE",
@@ -73,24 +70,15 @@ impl UbxCfgRate {
 
     pub fn load(&mut self, data: &[u8]) {
         assert!(data.len() == 6);
+        self.data = bincode::deserialize(&data).unwrap();
+        // println!("Decoded struct is {:?}", self.data);
 
-        // TODO: improve further, this can't be the best solution
-        self.meas_rate = u16::from_le_bytes([data[0], data[1]]);
-        self.nav_rate = u16::from_le_bytes([data[2], data[3]]);
-        self.time_ref = u16::from_le_bytes([data[4], data[5]]);
     }
 
     pub fn save(&self) -> Vec<u8> {
-        let mut data = Vec::with_capacity(6);
-        let bytes1 = self.meas_rate.to_le_bytes();
-        let bytes2 = self.nav_rate.to_le_bytes();
-        let bytes3 = self.time_ref.to_le_bytes();
-        // println!("{:?} {:?} {:?}" , bytes1, bytes2, bytes3);
-
-        data.append(&mut bytes1.to_vec());
-        data.append(&mut bytes2.to_vec());
-        data.append(&mut bytes3.to_vec());
+        let data = bincode::serialize(&self.data).unwrap();
         data
+
     }
 }
 
@@ -130,9 +118,9 @@ impl fmt::Debug for UbxCfgRate {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("UbxCfgRate")
         .field("cid", &self.cid)
-        .field("measRate", &self.meas_rate)
-        .field("navRate", &self.nav_rate)
-        .field("timeRef", &self.time_ref)
+        .field("measRate", &self.data.meas_rate)
+        .field("navRate", &self.data.nav_rate)
+        .field("timeRef", &self.data.time_ref)
         .finish()
     }
 }
@@ -156,9 +144,9 @@ mod tests {
         let mut dut = UbxCfgRate::new();
         dut.load(&DATA.to_vec());
 
-        assert_eq!(dut.meas_rate, 1000);
-        assert_eq!(dut.nav_rate, 1);
-        assert_eq!(dut.time_ref, 0x1234);
+        assert_eq!(dut.data.meas_rate, 1000);
+        assert_eq!(dut.data.nav_rate, 1);
+        assert_eq!(dut.data.time_ref, 0x1234);
     }
 
     #[test]
@@ -170,21 +158,21 @@ mod tests {
         dut.load(&DATA.to_vec());
         // println!("{:?}", dut);
 
-        assert_eq!(dut.meas_rate, 1000);
-        assert_eq!(dut.nav_rate, 1);
-        assert_eq!(dut.time_ref, 0x1234);
+        assert_eq!(dut.data.meas_rate, 1000);
+        assert_eq!(dut.data.nav_rate, 1);
+        assert_eq!(dut.data.time_ref, 0x1234);
     }
 
     #[test]
     fn cfg_rate_change() {
         let mut dut = UbxCfgRate::new();
-        assert_eq!(dut.meas_rate, 0);
-        assert_eq!(dut.nav_rate, 0);
-        assert_eq!(dut.time_ref, 0);
+        assert_eq!(dut.data.meas_rate, 0);
+        assert_eq!(dut.data.nav_rate, 0);
+        assert_eq!(dut.data.time_ref, 0);
 
-        dut.meas_rate = 1000;
-        dut.nav_rate = 1;
-        dut.time_ref = 0x1234;
+        dut.data.meas_rate = 1000;
+        dut.data.nav_rate = 1;
+        dut.data.time_ref = 0x1234;
         let data = dut.save();
         assert_eq!(data, [0xE8, 3, 1, 0, 0x34, 0x12]);
     }
@@ -192,13 +180,13 @@ mod tests {
     #[test]
     fn cfg_rate_serialize() {
         let mut dut = UbxCfgRate::new();
-        assert_eq!(dut.meas_rate, 0);
-        assert_eq!(dut.nav_rate, 0);
-        assert_eq!(dut.time_ref, 0);
+        assert_eq!(dut.data.meas_rate, 0);
+        assert_eq!(dut.data.nav_rate, 0);
+        assert_eq!(dut.data.time_ref, 0);
 
-        dut.meas_rate = 1000;
-        dut.nav_rate = 1;
-        dut.time_ref = 0x1234;
+        dut.data.meas_rate = 1000;
+        dut.data.nav_rate = 1;
+        dut.data.time_ref = 0x1234;
 
         let data = dut.to_bin();
         assert_eq!(data, [0xb5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xE8, 0x03, 0x01, 0x00, 0x34, 0x12, 70, 177]);
@@ -210,8 +198,8 @@ mod tests {
 
         let mut dut = UbxCfgRate::new();
         dut.load(&DATA);
-        assert_eq!(dut.meas_rate, 1000);
-        assert_eq!(dut.nav_rate, 1);
-        assert_eq!(dut.time_ref, 0x1234);
+        assert_eq!(dut.data.meas_rate, 1000);
+        assert_eq!(dut.data.nav_rate, 1);
+        assert_eq!(dut.data.time_ref, 0x1234);
     }
 }
