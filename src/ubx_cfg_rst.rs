9f240a381d9a473e4427e9a4b18711eaca3eea98
Rene Straub <straub@see5.ch>
2020-09-20 14:39:28 +0200

    Refactor UBX-CFG-RST to use enum types

--- src/ubx_cfg_rst.rs
+++ src/ubx_cfg_rst.rs
@@ -6,61 +6,47 @@ use crate::frame::{UbxFrame, UbxFrameInfo, UbxFrameSerialize};
 const CLS: u8 = 0x06;
 const ID: u8 = 0x04;
 
-/*
-TODO: Enum is serialized as four bytes, thus not working here
-See serde customization
-https://serde.rs/container-attrs.html
-https://serde.rs/enum-representations.html#untagged
+use serde_repr::Serialize_repr;
 
+#[derive(Serialize_repr, Debug)]
 #[repr(u16)]
-#[derive(Serialize, Debug)]
 pub enum BbrMask {
-    HOT_START = 0x0000,
-    WARM_START = 0x0001,
-    COLD_START = 0xFFFF,
+    HotStart = 0x0000,
+    // WarmStart = 0x0001,
+    ColdStart = 0xFFFF,
 }
 
 impl Default for BbrMask {
-    fn default() -> Self { BbrMask::HOT_START }
+    fn default() -> Self {
+        BbrMask::ColdStart
+    }
 }
 
-
+#[derive(Serialize_repr, Debug)]
 #[repr(u8)]
-#[derive(Serialize, Debug)]
 pub enum ResetMode {
-    IMMEDIATE_HW_RESET = 0x00,
-    SW_RESET = 0x01,
-    HW_RESET = 0x04,
-    STOP = 0x08,
-    START = 0x09,
+    ImmediateHwReset = 0x00,
+    SwReset = 0x01,
+    // HwReset = 0x04,
+    Stop = 0x08,
+    // Start = 0x09,
 }
 
 impl Default for ResetMode {
-    fn default() -> Self { ResetMode::IMMEDIATE_HW_RESET }
+    fn default() -> Self {
+        ResetMode::ImmediateHwReset
+    }
 }
-*/
-
-const HOT_START: u16 = 0x0000;
-// const WARM_START: u16 = 0x0001;
-const COLD_START: u16 = 0xFFFF;
-
-// const IMMEDIATE_HW_RESET: u8 = 0x00;
-const SW_RESET: u8 = 0x01;
-// const HW_RESET: u8 = 0x04;
-const STOP: u8 = 0x08;
-// const START: u8 = 0x09;
 
 #[derive(Default, Debug, Serialize)]
 pub struct Data {
-    // pub nav_bbr_mask: BbrMask,
-    // pub reset_mode: ResetMode,
-    pub nav_bbr_mask: u16,
-    pub reset_mode: u8,
+    pub nav_bbr_mask: BbrMask,
+    pub reset_mode: ResetMode,
     pub res1: u8,
 }
 
 impl Data {
-    pub fn new(mask: u16, reset_mode: u8) -> Self {
+    pub fn new(mask: BbrMask, reset_mode: ResetMode) -> Self {
         Self {
             nav_bbr_mask: mask,
             reset_mode: reset_mode,
@@ -81,7 +67,7 @@ impl UbxCfgRstAction {
         Self {
             name: "UBX-CFG-RST",
             cid: UbxCID::new(CLS, ID),
-            data: Data::new(COLD_START, SW_RESET),
+            data: Data::new(BbrMask::ColdStart, ResetMode::SwReset),
         }
     }
 
@@ -89,34 +75,13 @@ impl UbxCfgRstAction {
         Self {
             name: "UBX-CFG-RST",
             cid: UbxCID::new(CLS, ID),
-            data: Data::new(HOT_START, STOP),
+            data: Data::new(BbrMask::HotStart, ResetMode::Stop),
         }
     }
 
-    // TODO: Realize the following as constructors
-    // simper to use, just a bit more code here
-    /*
-        pub fn warm_start(&mut self) {
-            self.data.reset_mode = SW_RESET;
-            self.data.nav_bbr_mask = WARM_START;
-        }
-    */
-
-    /*
-        pub fn start(&mut self) {
-            self.data.reset_mode = START;
-            self.data.nav_bbr_mask = HOT_START;
-        }
-
-        pub fn stop(&mut self) {
-            self.data.reset_mode = STOP;
-            self.data.nav_bbr_mask = HOT_START;
-        }
-    */
-
     fn save(&self) -> Vec<u8> {
         let data = bincode::serialize(&self.data).unwrap();
-        assert!(data.len() == 4);
+        //assert!(data.len() == 4);
         data
     }
 }
@@ -140,7 +105,6 @@ impl UbxFrameSerialize for UbxCfgRstAction {
         let frame = UbxFrame::construct(UbxCID::new(CLS, ID), data);
         let msg = frame.to_bytes();
         msg
-        // TODO: Combine to one statement
     }
 }
 
