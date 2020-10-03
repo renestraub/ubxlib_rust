9b5942ee68547781943d397822493edebad43bc5
Rene Straub <straub@see5.ch>
2020-10-03 17:05:48 +0200

    Refactor UbxCfgCfgAction to use generic UbxFrame

--- src/ubx_cfg_cfg.rs
+++ src/ubx_cfg_cfg.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 
 use crate::cid::UbxCID;
-use crate::frame::{UbxFrame, UbxFrameInfo, UbxFrameSerialize};
+use crate::frame::UbxFrameWithData;
 
 const CLS: u8 = 0x06;
 const ID: u8 = 0x09;
@@ -16,7 +16,6 @@ pub struct Data {
 }
 
 impl Data {
-    // TODO: Remove load mask as not needed so far
     pub fn new(clear: u32, save: u32, load: u32) -> Self {
         Self {
             clear_mask: clear,
@@ -27,56 +26,27 @@ impl Data {
     }
 }
 
-#[derive(Default, Debug)]
-pub struct UbxCfgCfgAction {
-    pub name: &'static str,
-    cid: UbxCID,
-    pub data: Data,
-}
+pub struct UbxCfgCfgAction { }
 
-impl UbxCfgCfgAction {
-    pub fn factory_reset() -> Self {
-        Self {
-            name: "UBX-CFG-CFG",
-            cid: UbxCID::new(CLS, ID),
-            data: Data::new(MASK_ALL, 0, MASK_ALL),
-        }
+impl UbxCfgCfgAction { 
+    pub fn factory_reset() -> UbxFrameWithData<Data> {
+        UbxFrameWithData::init("UBX-CFG-CFG", UbxCID::new(CLS, ID), Data::new(MASK_ALL, 0, MASK_ALL))
     }
 
-    pub fn persist() -> Self {
-        Self {
-            name: "UBX-CFG-RST",
-            cid: UbxCID::new(CLS, ID),
-            data: Data::new(0, MASK_ALL, 0),
-        }
-    }
-}
-
-impl UbxFrameInfo for UbxCfgCfgAction {
-    fn name(&self) -> String {
-        String::from(self.name)
-    }
-
-    fn cid(&self) -> UbxCID {
-        self.cid
-    }
-}
-
-impl UbxFrameSerialize for UbxCfgCfgAction {
-    fn to_bin(&self) -> Vec<u8> {
-        let data = bincode::serialize(&self.data).unwrap();
-        assert_eq!(data.len(), 12);
-        UbxFrame::bytes(UbxCID::new(CLS, ID), data)
+    pub fn persist() -> UbxFrameWithData<Data> {
+        UbxFrameWithData::init("UBX-CFG-CFG", UbxCID::new(CLS, ID), Data::new(0, MASK_ALL, 0))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::frame::UbxFrameSerialize;
 
     #[test]
     fn factory_reset() {
         let dut = UbxCfgCfgAction::factory_reset();
+        assert_eq!(dut.name, "UBX-CFG-CFG");
         let msg = dut.to_bin();
         assert_eq!(
             msg[0..18],
@@ -85,12 +55,12 @@ mod tests {
                 0x1f, 0x1f, 0x00, 0x00
             ]
         );
-        //                      B5      62    06    09  0D 00  FF      FB    00    00    00    00    00    00    FF    FF    00    00   17  2B 7E
     }
 
     #[test]
     fn persist() {
         let dut = UbxCfgCfgAction::persist();
+        assert_eq!(dut.name, "UBX-CFG-CFG");
         let msg = dut.to_bin();
         assert_eq!(
             msg[..18],
@@ -99,6 +69,5 @@ mod tests {
                 0x00, 0x00, 0x00, 0x00
             ]
         );
-        //                       B5    62    06    09  0D 00    00    00    00    00    FF    FF    00    00    00    00    00    00   17   31 BF
     }
 }
