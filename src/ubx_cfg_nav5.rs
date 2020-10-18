84a04ca5e15cced0b784e3160adb85856719bafd
Rene Straub <straub@see5.ch>
2020-10-18 12:04:06 +0200

    ci: check test fail

--- src/ubx_cfg_nav5.rs
+++ src/ubx_cfg_nav5.rs
@@ -52,6 +52,7 @@ mod tests {
 
     #[test]
     fn poll() {
+        panic!("fail by intent");
         let dut = UbxCfgNav5Poll::new();
         assert_eq!(dut.name, "UBX-CFG-NAV5-POLL");
         let msg = dut.to_bin();
